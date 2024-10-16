1db3f15f466297cd4473ab48b008964ba778dfdd
Al Bundy <gale.s.salazar22@gmail.com>
2024-10-16 15:07:47 -0500

    added env file

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
+
+console.log('MongoDB URI:', process.env.MONGODB_URI);
 
 mongoose.connect(process.env.MONGODB_URI) || 'mongodb://127.0.0.1:27017/lashed_up_beauty';
 
