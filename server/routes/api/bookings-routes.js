b822e8779b3ee9b0c3856e2c2e9a0223f56b29c4
Al Bundy <gale.s.salazar22@gmail.com>
2024-10-16 13:09:21 -0500

    added getallclients

--- server/routes/api/bookings-routes.js
+++ server/routes/api/bookings-routes.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 // imports controller functions
 const {
     getSingleClient,
+    getAllClients,
     createBooking,
     updateBooking,
     deleteBooking     
@@ -12,6 +13,7 @@ const {
 
 // sets up crud
 // sets up get route at /clients/:id/:clientName, when a get request hits this endpoint, it will invoke the getsingleclient function using id and clientname as route parameters
+router.get('/clients', getAllClients);
 router.get('/clients/:id/:clientName', getSingleClient);
 router.post('/bookings', createBooking);
 router.put('/bookings/:id', updateBooking);
