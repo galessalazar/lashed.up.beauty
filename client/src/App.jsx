59dc0bef13937d134bebd4b07215b4efbd4c6f5f
Al Bundy <gale.s.salazar22@gmail.com>
2024-10-17 09:18:11 -0500

    removed images from navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,12 @@
 import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
     <div>
+      <Navbar />
       <Hero />
+      
     </div>
   );
 };
