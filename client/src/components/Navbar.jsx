5a2a5546a7028d65d95450fb5ba3eef94b4789a8
Al Bundy <gale.s.salazar22@gmail.com>
2024-10-22 20:32:06 -0500

    cleaned up navbar code

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -47,87 +47,37 @@ const Navbar = () => {
               </svg>
             </button>
           </div>
-          {/* <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-            <div className="flex flex-shrink-0 items-center"> */}
-              {/* <img
-                className="h-1 w-auto"
-                src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=500"
-                alt="Your Company"
-              /> */}
-            {/* </div> */}
-            <div className="hidden sm:ml-6 sm:block">
-              <div className="flex space-x-4">
-                <a
-                  href="#"
-                  className="rounded-md bg-gray-900 px-3 py-1 text-sm font-medium text-white"
-                  aria-current="page"
-                >
-                  About
-                </a>
-                <a
-                  href="#"
-                  className="rounded-md px-3 py-1 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-                >
-                  Services
-                </a>
-                <a
-                  href="#"
-                  className="rounded-md px-3 py-1 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-                >
-                  Scheduling
-                </a>
-                <a
-                  href="#"
-                  className="rounded-md px-3 py-1 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-                >
-                  Contact Me
-                </a>
-              {/* </div> */}
-            {/* </div> */}
-          </div>
-          {/* <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-            <button
-              type="button"
-              className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-            >
-              <span className="absolute -inset-1.5"></span>
-              <span className="sr-only">Photos</span>
-              <svg
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth="1.5"
-                stroke="currentColor"
-                aria-hidden="true"
-                data-slot="icon"
+
+          <div className="hidden sm:ml-6 sm:block">
+            <div className="flex space-x-4">
+              <a
+                href="#"
+                className="rounded-md bg-gray-900 px-3 py-1 text-sm font-medium text-white"
+                aria-current="page"
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0"
-                />
-              </svg>
-            </button> */}
+                About
+              </a>
+              <a
+                href="#"
+                className="rounded-md px-3 py-1 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+              >
+                Services
+              </a>
+              <a
+                href="#"
+                className="rounded-md px-3 py-1 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+              >
+                Scheduling
+              </a>
+              <a
+                href="#"
+                className="rounded-md px-3 py-1 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+              >
+                Contact Me
+              </a>
+            </div>
 
             <div className="relative ml-3">
-              {/* <div> */}
-                {/* <button
-                  type="button"
-                  className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                  id="user-menu-button"
-                  aria-expanded="false"
-                  aria-haspopup="true"
-                >
-                  <span className="absolute -inset-1.5"></span>
-                  <span className="sr-only">Reviews</span> */}
-                  {/* <img
-                    className="h-8 w-8 rounded-full"
-                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                    alt=""
-                  /> */}
-                {/* </button> */}
-              {/* </div> */}
-
               <div
                 className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
                 role="menu"
@@ -166,43 +116,9 @@ const Navbar = () => {
             </div>
           </div>
         </div>
-        
       </div>
-
-      </nav>
-
-      // <div className="sm:hidden" id="mobile-menu">
-      //   <div className="space-y-1 px-2 pb-3 pt-2">
-      //     <a
-      //       href="#"
-      //       className="block rounded-md bg-gray-900 px-3 py-1 text-base font-medium text-white"
-      //       aria-current="page"
-      //     >
-      //       Dashboard
-      //     </a>
-      //     <a
-      //       href="#"
-      //       className="block rounded-md px-3 py-1 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-      //     >
-      //       Team
-      //     </a>
-      //     <a
-      //       href="#"
-      //       className="block rounded-md px-3 py-1 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-      //     >
-      //       Projects
-      //     </a>
-      //     <a
-      //       href="#"
-      //       className="block rounded-md px-3 py-1 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-      //     >
-      //       Calendar
-      //     </a>
-      //   </div>
-      // </div>
-    // </nav>
+    </nav>
   );
 };
 
 export default Navbar;
-
