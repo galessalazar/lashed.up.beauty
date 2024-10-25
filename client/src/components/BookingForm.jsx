2820f95869e21b752be0411db7d2d55f9cd1f24e
Al Bundy <gale.s.salazar22@gmail.com>
2024-10-25 09:49:11 -0500

    added date&time to bookingform

--- client/src/components/BookingForm.jsx
+++ client/src/components/BookingForm.jsx
@@ -2,19 +2,24 @@
 
 import React, { useState } from "react";
 import ServiceSelection from "./ServiceSelection";
+import DateTimePicker from './DateTimePicker';
 
 const BookingForm = () => {
   const [selectedService, setSelectedService] = useState(null);
+  const [selectedDateTime, setSelectedDateTime] = useState(null);
 
   return (
     <div className="p-8 max-w-md mx-auto">
       <h1 className="text-2xl font-bold mb-8">Book Your Lash Appointment</h1>
       <ServiceSelection onServiceSelect={setSelectedService} />
-      {selectedService && (
+      <DateTimePicker onDateTimeSelect={setSelectedDateTime} />
+
+      {selectedService && selectedDateTime && (
         <div className="mt-4">
           <h2 className="text-lg font-semibold">You selected:</h2>
           <p>{selectedService.name}</p>
           <p>{selectedService.price}</p>
+          <p>Date & Time: {selectedDateTime.toLocaleString()}</p>
         </div>
       )}
     </div>
