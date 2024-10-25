b1bbf284a3e794e9a17837dc5d919ff570d43af6
Al Bundy <gale.s.salazar22@gmail.com>
2024-10-25 09:42:24 -0500

    added datetimepicker.jsx

---
+++ client/src/components/DateTimePicker.jsx
@@ -0,0 +1,31 @@
+import React, { useState} from 'react'
+import DatePicker from 'react-datepicker'
+import { newDate } from 'react-datepicker/dist/date_utils';
+import 'react-datepicker/dist/react-datepicker.css'
+
+
+
+const DateTimePicker = ({ onDateTimeSelect }) => {
+    const [startDate, setStartDate] = useState(null);
+
+    const handleDateChange = (date) => {
+        setStartDate(date);
+        onDateTimeSelect(date);
+    }
+  return (
+    <div className='mt-4'>
+   <h2 className='text-lg font-semibold mb-2'>Select Appointment Date and Time</h2>
+   <DatePicker 
+   selected={startDate} 
+   onChange={handleDateChange} 
+   showTimeSelect 
+   dateFormat='Pp' 
+   timeIntervals={30} 
+   minDate={new Date()} 
+   className='border p-2 rounded w-full' placeholderText='Select date and time'
+   />
+   </div>
+  );
+};
+
+export default DateTimePicker;
