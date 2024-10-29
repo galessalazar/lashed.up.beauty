c7cde62e21963eb9176b7ee9ff5539050940298d
Al Bundy <gale.s.salazar22@gmail.com>
2024-10-28 22:25:15 -0500

    updated index.js

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,9 +1,11 @@
 // creates new instance of an express router
-const router = require('express').Router();
-const bookingsRoutes = require('./bookings-routes');
-const servicesRoutes = require('./services-routes');
+const router = require("express").Router();
+const bookingsRoutes = require("./bookings-routes");
+const servicesRoutes = require("./services-routes");
+const clientRoutes = require("./client-routes");
 
-router.use('/bookings', bookingsRoutes);
-router.use('/services', servicesRoutes);
+router.use("/bookings", bookingsRoutes);
+router.use("/services", servicesRoutes);
+router.use('/clients', clientRoutes);
 
 module.exports = router;
