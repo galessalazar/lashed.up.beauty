b6f61a347226b3a8b1365903e460e163aa5131ca
Al Bundy <gale.s.salazar22@gmail.com>
2024-10-29 15:20:10 -0500

    separated client/bookings routes

--- server/routes/api/bookings-routes.js
+++ server/routes/api/bookings-routes.js
@@ -5,8 +5,7 @@ const router = require("express").Router();
 
 // imports controller functions
 const {
-  getSingleClient,
-  getAllClients,
+ 
   createBooking,
   updateBooking,
   deleteBooking,
@@ -16,11 +15,8 @@ const {
 
 // import middleware for authentication here when ready
 
-// sets up crud
-// sets up get route at /clients/:id/:clientName, when a get request hits this endpoint, it will invoke the getsingleclient function using id and clientname as route parameters
-router.get("/clients", getAllClients);
-router.get("/clients/:id/:clientName", getSingleClient);
-// i likely need to add this GETALLBOOKINGS UP TOP
+
+
 router.get("/", getAllBookings);
 router.get('/:id', getBookingById);
 router.post("/", createBooking);
--- server/routes/api/client-routes.js
+++ server/routes/api/client-routes.js
@@ -1,3 +1,8 @@
+// sets up crud
+// sets up get route at /clients/:id/:clientName, when a get request hits this endpoint, it will invoke the getsingleclient function using id and clientname as route parameters
+// router.get("/clients", getAllClients);
+// router.get("/clients/:id/:clientName", getSingleClient);
+
 const router = require('express').Router();
 const { getAllClients, getSingleClient } =  require ('../../controllers/user-controller');
 
