84de3b24c5db27c334c0e27c748635729a8fa421
Al Bundy <gale.s.salazar22@gmail.com>
2024-11-02 21:40:18 -0500

    reached server w bookings

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,8 +1,11 @@
 const mongoose = require('mongoose');
 require('dotenv').config({path: './server/.env'});
 
-console.log('MongoDB URI:', process.env.MONGODB_URI);
+console.log('MongoDB URI:', process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/lashed_up_beauty');
 
-mongoose.connect(process.env.MONGODB_URI) || 'mongodb://127.0.0.1:27017/lashed_up_beauty';
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/lashed_up_beauty', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
 
 module.exports = mongoose.connection;
