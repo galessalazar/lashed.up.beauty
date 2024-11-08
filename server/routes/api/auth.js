ba859feaf87d52c17470238dea6b42a0068c19ac
Al Bundy <gale.s.salazar22@gmail.com>
2024-11-08 12:10:19 -0600

    added auth.js

--- server/routes/api/auth.js
+++ server/routes/api/auth.js
@@ -1,4 +1,59 @@
 const express = require('express');
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
-const User = require('../../models/')
+const User = require('../../models/User');
+const router = express.Router();
+
+
+router.post('/register', async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ message: 'User already exists'});
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const newUser = new User({
+            email, 
+            password: hashedPassword,
+            role: 'Lash Technician',
+        });
+
+        await newUser.save();
+
+        const payload = { userId: newUser._id };
+        const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+        res.json({ token });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server error' });
+
+    }
+});
+
+router.post('/login', async (req, res) => {
+    const { email, password } req.body;
+
+    try {
+        const user = await User.findOne({ email });
+        if(!user) {
+            return res.status(400).json({ message: 'Invalid email or password' });
+        }
+
+        const payload = { userId: user._id };
+        const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h'});
+
+        res.json({ token });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server error' });
+    }
+});
+
+module.exports = router;
