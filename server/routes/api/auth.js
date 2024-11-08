a7a5462dcfdbf51a18202b71e71e33880bc0b028
Al Bundy <gale.s.salazar22@gmail.com>
2024-11-08 14:57:44 -0600

    loginform.jsx

--- server/routes/api/auth.js
+++ server/routes/api/auth.js
@@ -1,76 +1,74 @@
-const express = require('express');
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
-const User = require('../../models/User');
+require("dotenv").config();
+
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../../models/User");
 const router = express.Router();
 
 // register a new user (tech)
-router.post('/register', async (req, res) => {
-    console.log(req.body);
-    const { email, password } = req.body;
+router.post("/register", async (req, res) => {
+  console.log(req.body);
+  const { email, password } = req.body;
 
-    try {
-        const existingUser = await User.findOne({ email });
-        if (existingUser) {
-            return res.status(400).json({ message: 'User already exists'});
-        }
-
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-        const newUser = new User({
-            email, 
-            password: hashedPassword,
-            role: 'Lash Technician',
-        });
-
-        await newUser.save();
-
-        res.status(201).json({ msg: 'User registered successfully' });
-
-        const payload = { userId: newUser._id };
-        const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' });
-
-        res.json({ token });
-
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Server error' });
-
+  try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
     }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = new User({
+      email,
+      password: hashedPassword,
+      role: "Lash Technician",
+    });
+
+    await newUser.save();
+
+    res.status(201).json({ msg: "User registered successfully" });
+
+    const payload = { userId: newUser._id };
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    res.json({ token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
 });
 
-router.post('/login', async (req, res) => {
-    const { email, password } = req.body;
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
 
-    // login route for tech
+  // login route for tech
 
-
-    try {
-
-        // checks if  user exists
-        const user = await User.findOne({ email });
-        if(!user) {
-            return res.status(400).json({ message: 'Invalid email or password' });
-        }
-// compares the password entered with the hashed password in DB
-        const isMatch = await bcrypt.compare(password, user.password);
-        if(!isMatch) {
-            return res.status(400).json({ msg: 'Invalid credentials'});
-        }
-
-        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-            expiresIn: '1h',
-        });
-
-       
-// send back token
-        res.json({ token });
-
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Server error' });
+  try {
+    // checks if  user exists
+    const user = await User.findOne({ email });
+    console.log("user found:", user);
+    if (!user) {
+      return res.status(400).json({ message: "Invalid email or password" });
     }
-});
+    // compares the password entered with the hashed password in DB
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ msg: "Invalid credentials" });
+    }
 
+    const token = jwt.sign({ userId: user._id, role: user.role }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+
+    // send back token
+    res.json({ token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
 
 module.exports = router;
