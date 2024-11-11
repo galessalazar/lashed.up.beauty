2888eaf1ee05317ee65626e8a950093cb909658c
Al Bundy <gale.s.salazar22@gmail.com>
2024-11-11 12:49:05 -0600

    beauty button

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -40,45 +40,10 @@ const Navbar = () => {
               >
                 Contact Me
               </Link>
+
+              <Link to='/login' className="rounded-md px-3 py-1 text sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Login</Link>
             </div>
 
-            <div className="relative ml-3">
-              <div
-                className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-                role="menu"
-                aria-orientation="vertical"
-                aria-labelledby="user-menu-button"
-                tabIndex="-1"
-              >
-                {/* <Link
-                  to="#"
-                  className="block px-4 py-1 text-sm text-gray-700"
-                  role="menuitem"
-                  tabIndex="-1"
-                  id="user-menu-item-0"
-                >
-                  Your Profile
-                </Link>
-                <Link
-                  to="#"
-                  className="block px-4 py-1 text-sm text-gray-700"
-                  role="menuitem"
-                  tabIndex="-1"
-                  id="user-menu-item-1"
-                >
-                  Settings
-                </Link>
-                <Link
-                  to="#"
-                  className="block px-4 py-1 text-sm text-gray-700"
-                  role="menuitem"
-                  tabIndex="-1"
-                  id="user-menu-item-2"
-                >
-                  Sign out
-                </Link> */}
-              </div>
-            </div>
           </div>
         </div>
       </div>
