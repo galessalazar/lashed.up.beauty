dc9158e5f7b74af360fc612cb7c1034cbbb1718d
Al Bundy <gale.s.salazar22@gmail.com>
2024-11-18 12:00:22 -0600

    updated atlas

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,6 +3,15 @@ require('dotenv').config({path: './server/.env'});
 
 console.log('MongoDB URI:', process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/lashed_up_beauty');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/lashed_up_beauty')
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/lashed_up_beauty', { 
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => {
+        console.log('Connected to MongoDB Atlas');
+    })
+    .catch((err) => {
+        console.error('Error connecting to MongoDB:', err);
+    });
 
 module.exports = mongoose.connection;
