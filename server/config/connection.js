8fb994eb0f6b8af283bd84a4a1faebc3abecfbd7
Al Bundy <gale.s.salazar22@gmail.com>
2024-11-25 09:36:40 -0600

    added db name to env

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,6 +3,8 @@ require('dotenv').config({path: './server/.env'});
 
 console.log('MongoDB URI:', process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/lashed_up_beauty');
 
+// establishes connection to atlas when the app starts
+
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/lashed_up_beauty', { 
     useNewUrlParser: true,
     useUnifiedTopology: true,
