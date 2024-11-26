b7aa8d384f2768e879ebe240c46d0d432f9c76bd
Al Bundy <gale.s.salazar22@gmail.com>
2024-11-26 13:58:28 -0600

    removed try/catch on auth

--- server/routes/api/auth.js
+++ server/routes/api/auth.js
@@ -8,7 +8,7 @@ const router = express.Router();
 
 // register a new user (tech)
 router.post("/register", async (req, res) => {
-  console.log('Incoming request body:', req.body);
+  console.log("Incoming request body:", req.body);
   const { email, password } = req.body;
 
   try {
@@ -26,33 +26,26 @@ router.post("/register", async (req, res) => {
     });
 
     await newUser.save();
-    console.log('User saved:', newUser);
+    console.log("User saved:", newUser);
 
     const payload = { userId: newUser._id, role: newUser.role };
-     const token = jwt.sign(payload, process.env.JWT_SECRET, {
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
       expiresIn: "1h",
     });
 
     res.status(201).json({ msg: "User registered successfully", token });
-
-   
-
   } catch (error) {
-    console.error('Error saving user:', error);
+    console.error("Error saving user:", error);
     res.status(500).json({ message: "Server error" });
   }
 });
 
 router.post("/login", async (req, res) => {
-  try {
-  console.log('Received login request:', req.body);
-
   const { email, password } = req.body;
-  console.log('Incoming request body:', req.body);
+  console.log("Incoming request body:", req.body);
 
-  } catch (error) {
-    console.error('Error parsing request body:', error);
-    res.status(500).json({ message: 'Server error'});
+  if (!email || !password) {
+    return res.status(400).json({ message: "Missing email or password" });
   }
 
   // login route for tech
@@ -70,15 +63,19 @@ router.post("/login", async (req, res) => {
       return res.status(400).json({ msg: "Invalid credentials" });
     }
 
-    const token = jwt.sign({ userId: user._id, role: user.role }, process.env.JWT_SECRET, {
-      expiresIn: "1h",
-    });
+    const token = jwt.sign(
+      { userId: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1h",
+      }
+    );
 
     // send back token
     res.json({ token });
   } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Server error" });
+    console.error("Error during login:", error);
+    res.status(500).json({ message: "Server error", error: error.message, stack: error.stack });
   }
 });
 
